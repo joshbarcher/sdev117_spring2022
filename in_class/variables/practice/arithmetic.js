09b2faf20a7842c487094521c11f3ef878e05661
Josh Archer <jarcher@greenriver.edu>
2022-04-12 14:47:14 -0700

    Added an example of parsing numbers.

---
+++ in_class/variables/practice/arithmetic.js
@@ -0,0 +1,19 @@
+//read in two values (as strings)
+let num1 = prompt("Enter a number");
+let num2 = prompt("Enter a number");
+
+//convert a string to a number using parseInt() or parseFloat()
+num1 = parseInt(num1); //convert the value in num1 to a number
+num2 = parseInt(num2); //convert the value in num2 to a number
+
+//add them together
+let resultAdd = num1 + num2;
+let resultSubtract = num1 - num2;
+let resultMultiply = num1 * num2;
+let resultDivide = num1 / num2;
+
+//show the result (using string interpolation)
+console.log(`The result of ${num1} + ${num2} = ${resultAdd}`);
+console.log(`The result of ${num1} - ${num2} = ${resultSubtract}`);
+console.log(`The result of ${num1} * ${num2} = ${resultMultiply}`);
+console.log(`The result of ${num1} / ${num2} = ${resultDivide}`);
